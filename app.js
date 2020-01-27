2c74a29c1ae5d9d5e474e9e42517aa8d347f05ee
Tim <tim.weinert@gmail.com>
2020-01-27 13:34:05 +1100

    app functional - comments to be updated

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const panels = document.querySelectorAll(".panel");
 
-function toggleOpen(){
+function toggleOpen() {
     //the element + class (div + .panel + .open)
     //This function will initial the css property for .panel-open in the element that is clicked
     this.classList.toggle('open');
 };
 
+
+function toggleActive(e) {
+    console.log(e.propertyName)
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active')
+    }
+}
+
 //loop over each panel and listen for a click. Initialize function when event takes place
-panels.forEach(panel => panel.addEventListener('click', toggleOpen))
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive))
