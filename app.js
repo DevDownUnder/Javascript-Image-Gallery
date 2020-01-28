5e3aa2877aba01795ff5bc387d1baad529de381b
Tim <tim.weinert@gmail.com>
2020-01-28 14:55:49 +1100

    comments updated

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const panels = document.querySelectorAll(".panel");
 
+//Open up the panel to 5 times it's width when clicked
 function toggleOpen() {
-    //the element + class (div + .panel + .open)
-    //This function will initial the css property for .panel-open in the element that is clicked
+    //This function will initialize the css property .panel-open in the element that is clicked
     this.classList.toggle('open');
 };
 
-
+//Toggle the transition the text that is hidden to show / close
 function toggleActive(e) {
-    console.log(e.propertyName)
+    debugger;
+    //if  event contains a class that includes flex 
     if (e.propertyName.includes('flex')) {
+        //toggle this (element clicked on) which contains class open active
         this.classList.toggle('open-active')
     }
 }
@@ -17,4 +19,5 @@ function toggleActive(e) {
 //loop over each panel and listen for a click. Initialize function when event takes place
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 
+//loop over each panel and listen for the  transition to end. Initialize function when event takes place
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive))
